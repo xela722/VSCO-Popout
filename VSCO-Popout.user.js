30ab462df0a5020da89752cca943a8326213ba4b
xela722 <alex0722@comcast.net>
2016-06-18 14:57:45 -0400

    Update VSCO-Popout.user.js
    Updated. Make sure you refresh on each new image

--- VSCO-Popout.user.js
+++ VSCO-Popout.user.js
@@ -7,24 +7,33 @@
 // @exclude     https://grid.vsco.co/feed*
 // @exclude     https://*.vsco.co/grid/*
 // @exclude     https://vsco.co/
-// @exclude     https://vsco.co/*/grid/*
+// @exclude     https://vsco.co/*/images/*
+// @include     http://*.vsco.co/*
+// @include     http://vsco.co/*
+// @exclude     http://grid.vsco.co/feed*
+// @exclude     http://*.vsco.co/grid/*
+// @exclude     http://vsco.co/
+// @exclude     http://vsco.co/*/images/*
 // @version     1
 // @grant       none
 // ==/UserScript==
-$(document).ready(function(){
-   location.reload();
-
-   function getVideoContent() { 
-      var metas = document.getElementsByTagName('meta'); 
-   
-      for (i=0; i<metas.length; i++) { 
-        if (metas[i].getAttribute("property") == "og:image") { 
-            var x = metas[i].getAttribute("content"); 
-         } 
-      } 
-     window.open(x)
-   } 
-   $(document).click(function(){
-      getVideoContent()
-   });
-)};
+function getVideoContent() {
+  var metas = document.getElementsByTagName('meta');
+  for (i = 0; i < metas.length; i++) {
+    if (metas[i].getAttribute('property') == 'og:image') {
+      var x = metas[i].getAttribute('content');
+    } else if (metas[i].getAttribute('property') == 'og:image:width') {
+      var y = metas[i].getAttribute('content');
+    } else if (metas[i].getAttribute('property') == 'og:image:height') {
+      var z = metas[i].getAttribute('content');
+    }
+  }
+  if (x.includes('w=')) {
+    window.open(x.replace('w=800', 'w=' + y));
+  } else if (x.includes('h=')) {
+    window.open(x.replace('h=800', 'h=' + z));
+  }
+}
+$(document).click(function () {
+  getVideoContent();
+});
